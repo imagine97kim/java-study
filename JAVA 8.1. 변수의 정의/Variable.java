89eae7fcf68f36aa7555e58ecdf22129bbf5d473
imagine <96781739+imagine97kim@users.noreply.github.com>
2022-06-09 18:27:15 +0900

    변수의 정의

---
+++ JAVA 8.1. 변수의 정의/Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int a = 1; // Number -> integer
+		System.out.println(a); // 1
+		
+//		int b = 1.1; // ERROR
+		double b = 1.1;
+		System.out.println(b); // 1.1
+		
+		String c = "Hello World";
+	    System.out.println(c); // Hello World
+	    
+	    
+
+	}
+
+}
