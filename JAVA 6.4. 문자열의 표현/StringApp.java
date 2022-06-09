c117c01701d07d794e5e6ff3d09529937b80243a
imagine <96781739+imagine97kim@users.noreply.github.com>
2022-06-09 12:57:06 +0900

    문자열의 표현

---
+++ JAVA 6.4. 문자열의 표현/StringApp.java
@@ -0,0 +1,17 @@
+public class StringApp {
+	public static void main(String[] args) {
+		
+		// String VS Character
+		System.out.println("Hello World"); // String
+//		System.out.println('Hello World'); // Error
+		System.out.println('H'); // Character
+		System.out.println("H"); // String
+		
+		// New Line
+		System.out.println("Hello \nWorld"); // 줄바꿈 문자
+		
+		// escape
+//		System.out.println("Hello "World""); // Hello "World" ?
+	    System.out.println("Hello \"World\""); // Hello "World"
+	}
+}
