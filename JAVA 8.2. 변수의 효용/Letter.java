98be4f598087d8763b01defdc02caf7910243107
imagine <96781739+imagine97kim@users.noreply.github.com>
2022-06-09 18:48:37 +0900

    변수의 효용

---
+++ JAVA 8.2. 변수의 효용/Letter.java
@@ -0,0 +1,19 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+        //1번 
+		System.out.println("Hello, imagine ... imagine ... bye."); // Hello, imagine ... imagine ... bye.
+		
+		String name = "egoing";
+		System.out.println("Hello, " + name +" ... " + name + " ... bye."); // Hello, egoing ... egoing ... bye.
+		
+		//2번 
+		System.out.println(10.0); // 10.0 ?
+		
+		double VAT = 10.0;
+		System.out.println(VAT); //VAT 10.0 !
+
+	}
+
+}
